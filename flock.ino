d3a08b9c2cdd7c182b5904b6f10fea560c6405c3
Will Simm <will@wasdesign.net>
2017-01-31 16:13:57 +0000

    add code

---
+++ flock.ino
@@ -0,0 +1,34 @@
+//80bpm
+ 
+int motorPin = 3;
+
+int bpm = 80;
+int bpmdelay=750; //  60000 (1 min) / 80
+int pulselength = 1;
+ 
+void setup() 
+{ 
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+  /*if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin, speed);
+    }
+  }*/
+
+
+ delay (bpmdelay-pulselength);
+ analogWrite(motorPin, 40);
+ delay(pulselength);
+  
+} 
